821710004d1e8181548bce920664b6e7ff0d195d
Qi Guo <uniqueguoqi@gmail.com>
2014-05-29 15:06:06 +0800

    few changes

--- letusgo/controllers/account.py
+++ letusgo/controllers/account.py
@@ -21,7 +21,7 @@ def login():
         raise ThrownError('Wrong telephone number')
     istimeout(args['dd'])
     # print user.tel, user.pwd, args['dd']
-    if hash(':'.join([user.tel, user.pwd, args['dd']])) != args['info']:
+    if hash(':'.join([user.tel, user.pwd, int(args['dd'])])) != args['info']:
         raise ThrownError('Wrong password')
     # login success, genrate token and return
     user.token = gentoken()
