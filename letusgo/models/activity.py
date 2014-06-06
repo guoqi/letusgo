51316619d6ce935c169527895345d90a0a6d0dda
Qi Guo <uniqueguoqi@gmail.com>
2014-06-06 12:39:21 +0800

    modify interface

--- letusgo/models/activity.py
+++ letusgo/models/activity.py
@@ -83,7 +83,7 @@ class Activity(db.Model):
         return {
                     'aid': self.aid, 
                     'name': self.name, 
-                    'host': self.host.dump(), 
+                    'host': self.host.uid, 
                     'intro': self.intro, 
                     'launch_t': gettimestamp(self.launch_t), 
                     'start_t': gettimestamp(self.start_t), 
