af931b61412a0cc754317f5bf49b37550a384616
root <root@www.uniqueguoqi.com>
2014-06-07 09:13:32 -0400

    little changes

--- letusgo/models/review.py
+++ letusgo/models/review.py
@@ -6,7 +6,7 @@ from _base import db
 
 class Review(db.Model):
     rid = db.Column(db.Integer, primary_key=True, nullable=False)
-    content = db.Column(db.String(65535), nullable=False)
+    content = db.Column(db.String(255), nullable=False)
     # use review.activity
     aid = db.Column(db.Integer, db.ForeignKey('activity.aid'), nullable=False)
     # use review.reviewer
